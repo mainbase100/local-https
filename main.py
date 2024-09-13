2712623d22f35e3fd5992622b466481be21e8224
mainbase100 <youshud86@gmail.com>
2024-09-13 21:20:51 +0900

    first

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+
+# 初期化
+app = Flask(__name__)
+
+# ルートアクセス時の処理
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
